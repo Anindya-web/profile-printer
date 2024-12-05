da8465eed5d7d23fb709716fb21c717bc2b08b3e
Anindya Das <158485024+Anindya-web@users.noreply.github.com>
2024-12-06 00:57:29 +0600

    Add files via upload

---
+++ profile_printer.py
@@ -0,0 +1,7 @@
+name = input("Please, Enter your name: ")
+age = int(input("Please, Enter your age: "))
+favorite_hobby = input("Please, Enter your favorite hobby name: ")
+
+print(f"The name is:", name)
+print(f"The age is:", age)
+print(f"The favorite hobby is:", favorite_hobby)
